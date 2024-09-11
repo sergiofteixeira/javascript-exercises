fe4be207f4cf7fdd0d3aad9ae5b30b640d6f342e
Sergio Teixeira <sergio@triggerise.org>
2024-09-11 17:42:05 +0100

    complete ex 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...args) {
+  const newArray = [];
 
+  console.log(array.length)
+  for (let i = 0; i < array.length; i++) {
+    if (!args.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray
+}
+
+console.log(removeFromArray([1, 2, 2, 3], 2, 3, 4))
 // Do not edit below this line
 module.exports = removeFromArray;
