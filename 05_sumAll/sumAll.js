3bbe55220d6a654936277aa82bbf756f9134ee77
Sergio Teixeira <sergio@triggerise.org>
2024-09-12 10:12:56 +0100

    finished 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,30 +1,29 @@
 function sumAll(a, b) {
-  let total = 0
+    let total = 0
 
-  if (Number.isInteger(a) && Number.isInteger(b)) {
-    if (a > 0 && b > 0) {
-      if (b > a) {
-        while (a <= b) {
-          total = total + a
-          a++
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a > 0 && b > 0) {
+            if (b > a) {
+                while (a <= b) {
+                    total = total + a
+                    a++
+                }
+                return total
+            } else if (a > b) {
+                while (b <= a) {
+                    total = total + b
+                    b++
+                }
+                return total
+            } else {
+                return "ERROR"
+            }
+        } else {
+            return "ERROR"
         }
-        return total
-      } else if (a > b) {
-        while (b <= a) {
-          total = total + b
-          b++
-        }
-        return total
-      } else {
-        return "ERROR"
-      }
     } else {
-      return "ERROR"
+        return "ERROR"
     }
-  } else {
-    return "ERROR"
-  }
-  return total
 }
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
-
+function leapYears(year) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 4 === 0 && year % 400 === 0) {
+        console.log("leap year")
+        return true
+    } else {
+        console.log("not a leap year")
+        return false
+    }
+}
 // Do not edit below this line
 module.exports = leapYears;
