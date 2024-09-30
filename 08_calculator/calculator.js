b5428f687b8730b83a5d9558d5b8d671cacf5b38
Sergio Teixeira <sergio@triggerise.org>
2024-09-30 16:49:27 +0100

    solve 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return (array.reduce(add, 0))
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  total = array.reduce((arg, item) => (arg * item), 1);
+  return total
+};
+multiply([1, 2, 3])
 
+const power = function (a, b) {
+  return a ** b
 };
 
-const power = function() {
-	
-};
+const factorial = function (num) {
+  result = num;
+  if (num < 0) {
+    return -1
+  }
+  if (num === 0 || num === 1) {
+    return 1
+  }
+  while (num > 1) {
+    num--
+    result = result * num
+  }
+  return result
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
