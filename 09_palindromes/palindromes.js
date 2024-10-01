bb77ca847360b3e057a9dfbbf670a8633926265a
Sergio Teixeira <sergio@triggerise.org>
2024-10-01 21:36:07 +0100

    solve 10 calculator

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,15 @@
 const palindromes = function (string) {
-    splitString = string.split("")
-    reverseString = splitString.reverse()
-    reverseWord = reverseString.join("")
-    if (string === reverseWord) {
-        console.log("true")
-    } else {
-        console.log("false")
-    }
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedString;
 
 };
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+    if (input < 0) {
+        return "OOPS"
+    }
+    fib = [0, 1]
+    for (let index = fib.length; index < 100; index++) {
+        fib[index] = fib[index - 2] + fib[index - 1]
+    }
+    return fib[input]
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
