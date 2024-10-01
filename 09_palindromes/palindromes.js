37130913a0e7ab605c20664fe372e71094c585cc
Sergio Teixeira <sergio@triggerise.org>
2024-10-01 21:09:17 +0100

    solve 09 calculator

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    splitString = string.split("")
+    reverseString = splitString.reverse()
+    reverseWord = reverseString.join("")
+    if (string === reverseWord) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
